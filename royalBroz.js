d6400e8d8bbfadf990bc49a3934e52f1dcc6fa08
kanhaiyap <kanhaiyapandey396@gmail.com>
2020-03-31 07:00:46 +0530

    Initial commit

---
+++ royalBroz.js
@@ -0,0 +1,18 @@
+
+var img1= new Image()
+img1.src="slides/images.jpg"
+var img2= new Image()
+img2.src="slides/images (1).jpg"
+var img3= new Image()
+img3.src="slides/images (2).jpg"
+var img4= new Image()
+img4.src="slides/images (3).jpg"
+var img5= new Image()
+img5.src="slides/images (4).jpg"
+var img6= new Image()
+img6.src="slides/images (5).jpg"
+;
+
+function search(){
+alert('Site is under construction');
+};
